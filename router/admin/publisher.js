4294010d26e304de7c703ebc2acf79a0eca4888e
Lijo John <lijojohnrbs@gmail.com>
2021-11-28 21:08:04 +0530

    lkjdslkd

--- router/admin/publisher.js
+++ router/admin/publisher.js
@@ -10,12 +10,12 @@ const {
 
 admin_publisher.get("/admin/publishers", admin_middleware, publisher_list);
 admin_publisher.post(
-  "/admin/publsher/block:id",
+  "/admin/publsher/block/:id",
   admin_middleware,
   publisher_block
 );
 admin_publisher.post(
-  "/admin/publsher/unblock:id",
+  "/admin/publsher/unblock/:id",
   admin_middleware,
   publisher_unblock
 );
